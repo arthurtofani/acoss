c898414ff3291d0fbadb9e431ce4012086522cef
Arthur Tofani <gramofone@gmail.com>
2020-03-22 15:33:26 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -41,10 +41,10 @@ setup(
         'numpy>=1.16.5',
         'numba>=0.43.0',
         'pandas>=0.25.0',
-        'scipy==1.2.1',
-        'scikit-learn==0.19.2',
+        'scipy>=1.2.1',
+        'scikit-learn>=0.19.2',
         'deepdish>=0.3.6',
-        'librosa==0.6.1',
+        'librosa>=0.6.1',
         'progress>=1.5'
     ],
     extras_require={
